5421a3c06271b5801d49e11ddccf400b98493c14
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-25 12:37:16 -0500

    Rendering child props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
+import TodoList from "./TodoList"
 
 function App() {
+
+  const [todos,setTodo]=useState(["Todo1","Todo2"]);
+
+
   return (
     <>
-    <h1>Assalaamu aleykom</h1>
+    <TodoList todos={todos}/>
+    <input type="text"></input>
+    <button>Add todo</button>
+    <button>Clear todo</button>
+    <div>0 left to do</div>
     </>
   );
 }
